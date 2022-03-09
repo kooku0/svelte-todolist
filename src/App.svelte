0af580474765b7c2840b96baace1c8a68ac629a4
kooku <kooku@kakao.com>
2022-03-10 01:39:35 +0900

    feature: add, delete, complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,47 @@
 <script lang="ts">
-  export let name: string;
+  let id = 0;
+  let value: string;
+  let items = [];
+
+  function addItem() {
+    items = [...items, { id, value, complete: false }];
+    value = "";
+    id += 1;
+  }
+  function deleteItem(id) {
+    items = items.filter((item) => item.id !== id);
+  }
+  function completeItem(id) {
+    items = items.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          complete: !item.complete,
+        };
+      }
+
+      return {
+        ...item,
+      };
+    });
+  }
 </script>
 
 <main>
   <h1>TodoList</h1>
-  <div>{name}</div>
+  <input bind:value />
+  <button on:click={addItem}>click</button>
+  {#each items as { value, complete, id } (id)}
+    <div>
+      <span>{value}</span>
+      <span>
+        <button disabled={complete} on:click={() => completeItem(id)}>
+          complete
+        </button>
+        <button on:click={() => deleteItem(id)}>delete</button>
+      </span>
+    </div>
+  {/each}
 </main>
 
 <style>
